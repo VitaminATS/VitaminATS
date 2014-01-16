6d96fe06273c0615c5edce89eb8e4415b087c627
Ronald Cheng <rhc2104@columbia.edu>
2014-01-15 17:24:36 -0800

    Add rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VitaminATS::Application.config.secret_key_base = 'b12155903af4064958ba73a6b94af1f983f11cdde69b29a437538e70cc3957ff27ef7a82d154dc1e4535922b3fd9a18537c4c9aab968536dfddc047ee6cecf0b'
